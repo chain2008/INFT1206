7b297e4aaf265fd1eb6c3d81c3b36d719fa12c33
Cheng He <chenghe.gm@gmail.com>
2024-03-09 13:56:26 -0500

    i

--- assignment4/part1/content.js
+++ assignment4/part1/content.js
@@ -1,5 +1,7 @@
 
-const storyText  = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const roles = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const actions = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+function getStory(role,place,action,temperature='94 fahrenheit',client='Bob',weight='300 pounds'){
+    return `It was ${temperature} outside, so ${role} went for a walk. When they got to ${place}, they stared in horror for a few moments, then ${action}. ${client} saw the whole thing, but was not surprised — ${role} weighs ${weight}, and it was a hot day.`
+}
--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -2,38 +2,28 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
 
 randomize.addEventListener('click', result);
 
 function result() {
-    let newStory = storyText;
+  let role = randomValueFromArray(roles);
+  let place = randomValueFromArray(places);
+  let action = randomValueFromArray(actions);
+  let client,weight,temperature;
 
-    const xItem = randomValueFromArray(insertX);
-    const yItem = randomValueFromArray(insertY);
-    const zItem = randomValueFromArray(insertZ);
-
-    newStory = newStory
-                .replaceAll(":insertx:", xItem)
-                .replace(":inserty:", yItem)
-                .replace(":insertz:", zItem);
-
-  if(customName.value !== '') {
-    const name = customName.value;
-    newStory =newStory.replace("Bob", name);
+  if (customName.value !== '') {
+    client = customName.value;
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight =  `${Math.round(300 / 14)} Stone`;
-    const temperature =  `${Math.round((94 - 32)* 5/9)} centigrade`;
-    newStory = newStory
-                .replace('300 pounds', weight)
-                .replace('94 fahrenheit', temperature);
+  if (document.getElementById("uk").checked) {
+    weight = `${Math.round(300 / 14)} Stone`;
+    temperature = `${Math.round((94 - 32) * 5 / 9)} centigrade`;
   }
 
-  story.textContent = newStory;
+  story.textContent = getStory(role, place, action, temperature, client, weight);
   story.style.visibility = 'visible';
 }
