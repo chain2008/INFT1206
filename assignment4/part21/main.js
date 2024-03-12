53e0aeddc4374e7ac5187179506261df8909feb4
Cheng He <chenghe.gm@gmail.com>
2024-03-12 08:45:27 -0400

    i

--- assignment4/part21/main.js
+++ assignment4/part21/main.js
@@ -6,7 +6,12 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+const images = [
+  {file:'pic1.jpg', alt:'Human eye close up'}, 
+  {file:'pic2.jpg', alt:'Rock that resembles a wave'},
+  {file:'pic3.jpg', alt:'Purple and white flowers'},
+  {file:'pic4.jpg', alt:'Hieroglyphics of pharaoh'},
+  {file:'pic5.jpg', alt:'Moth on a leaf'}]
 
 /* Declaring the alternative text for each image file */
 
@@ -23,8 +28,8 @@ const alts = {
 for (const image of images) {
     const newItem = document.createElement('li');
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/${image}`);
-    newImage.setAttribute('alt', alts[image]);
+    newImage.setAttribute('src', `images/${image.file}`);
+    newImage.setAttribute('alt', `images/${image.alt}`);
     newItem.appendChild(newImage);
     thumbBar.appendChild(newItem);
     newImage.addEventListener('click', e => {
