b85b84dc2df0051f419934c288b3ec761eee7490
Cheng He <chenghe.gm@gmail.com>
2024-03-16 16:40:06 -0400

    good

--- learn/object/prototype.js
+++ learn/object/prototype.js
@@ -1,11 +1,11 @@
 const Person = {
     //name: ["Bob", "Smith"],
     //birthday: new Date(),
-    age(){
+    age: function(){ //cannot be arrow function
         //let birthday = new Date(_birthday);
         return (new Date()).getFullYear() - this.birthday.getFullYear();
     },
-    bio() {
+    bio: function(){
         console.log(`${this.name[0]} ${this.name[1]} is ${this.age()} years old.`);
     },
     introduceSelf() {
@@ -15,10 +15,9 @@ const Person = {
 
 function Student(name, birthday) {
     this.name = ["Bob", "Smith"];
-    this.birthday = new Date(birthday)
-    //this.Prototype.birthday = new Date(birthday);
+    this.birthday = new Date(birthday);
 };
-
+//we only prototype to reuse function
 Object.assign(Student.prototype , Person);
 
 let smith = new Student('smith', '2000/01/01');
